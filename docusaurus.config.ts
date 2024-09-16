3151c7006b3929f2af7db0c05853552883abd336
paper <faition2@gmail.com>
2024-09-16 16:42:43 +0800

    fix: use custom domain

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   favicon: 'img/ico-star.png',
 
   // Set the production url of your site here
-  url: 'https://wen0720.github.io',
+  url: 'https://papersuniverse.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/blog/',
